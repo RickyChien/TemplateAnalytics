40a1e1a6118e259015cd9a3ad7882dae17978d0a
Ricky Chien <ricky060709@gmail.com>
2013-11-25 22:47:33 +0800

    Add elements link

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,10 @@
 (function() {
   $('#start-time').datetimepicker();
   $('#end-time').datetimepicker();
-  
+  $.ajax({
+    data: {format: "json"},
+    url: "http://headers.jsontest.com/"
+  }).done(function(data) {
+    GridView.updateView(data);
+  });
 }());
