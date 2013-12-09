e52ab5413e1b7696dc32eb17d395fb417da19098
Ricky Chien <ricky060709@gmail.com>
2013-12-09 22:52:18 +0800

    Add sinon.js but test suites need to fix

--- test/spec/controller/controller.test.js
+++ test/spec/controller/controller.test.js
@@ -16,6 +16,14 @@ describe('Controller', function () {
   describe('initialize', function() {
     before(function() {
       this.controller.initialize();
+      this.searchBtn = document.querySelector('#search-btn');
+      this.searchBtn.click();
+
+      this.stubAddEventListener = sinon.stub(this.searchBtn, 'click');
+    });
+
+    after(function() {
+      this.stubAddEventListener.restore();
     });
 
     it('should setup global paceOptions', function() {
@@ -24,8 +32,13 @@ describe('Controller', function () {
       expect(window.paceOptions).to.have.property('restartOnRequestAfter', 10);
     });
 
+    it('should set hash url when default search button click', function() {
+      expect(this.stubAddEventListener.called).to.be.true;
+    });
+
     it('should attach "hashchange" event to window', function() {
-      
+      expect(window.location.hash).to.equal('#push_notification');
     });
   });
+
 });
